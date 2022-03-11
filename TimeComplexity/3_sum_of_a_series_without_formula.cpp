552ebf1e211518a9b54b4cb8de9e9795249facf9
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-12 00:55:15 +0600

    TimeComplexity/3_sum_of_a_series_without_formula: create

---
+++ TimeComplexity/3_sum_of_a_series_without_formula.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, result = 0;
+
+    cin>> n;
+
+    for(int i = 0; i < n; i++){
+        result = result + i; /* here number of operation is dependent on input, if the input is 5 then the loop will execute 5 times */
+    }
+    cout << result; /* this is not a complexity, because, it'll execute once */
+
+    return 0;
+}
+
+/*
+    In this program total operation number is n. Beacause, here number of operation is dependent on input.
+    Input is not constanst. So, time complexity of this program is Big O(n) or O(n) or Order(n) or linear.
+    Time complexity of a program will always Big O(n) or O(n) or Order(n), when the number of operation is dependent on input of a program.
+*/
