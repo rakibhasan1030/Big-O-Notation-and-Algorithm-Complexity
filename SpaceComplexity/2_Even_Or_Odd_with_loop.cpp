d4a9aabf3174908a875b7c92948e39b3210227da
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-12 00:17:08 +0600

    SpaceComplexity/2_Even_Or_Odd_with_loop: update

--- SpaceComplexity/2_Even_Or_Odd_with_loop.cpp
+++ SpaceComplexity/2_Even_Or_Odd_with_loop.cpp
@@ -1,9 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
-    
+    int i, n, nums[101];
 
-    
+    cin>>n;
+
+    for(int i = 0; i < 101; i++){
+        nums[i] = 0;
+    }
+
+    for(int i = 0; i < 101; i+=2){
+        nums[i] = 1;
+    }
+
+    if(nums[n]){
+        cout<< n << " is EVEN!" << endl;
+    }else{
+        cout<< n << " is ODD!" << endl;
+    }
+
     return 0;
 } /* Space Complexity of this program is Big O(n) or O(n) or Order(n)*/
