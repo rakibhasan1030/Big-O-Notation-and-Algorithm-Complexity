5a31261b417e63ebbfe790ffc4dd7e76c31243df
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-11 23:44:57 +0600

    TimeComplexity/1_sum_of_integer: create

---
+++ TimeComplexity/1_sum_of_integer.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m, n, result;
+
+    m = 5;          // first assignment operatoion
+    n = 10;         // second assignment operatoion
+    result = m + n; // third assignment operatoion and a mathmatical operation which is sum of (m+n)
+
+    cout << result;
+
+    return 0;
+}
+
+/*
+    In this program total operation is 4 and It'll be always 4. Beacause, here number of operation is not dependent on input.
+    Input is constanst. So, time complexity of this program is O(1).
+    Time complexity of a program will always Big O(1) or O(1) or Order(1), when the number of operation is not dependent on input of a program.
+*/
